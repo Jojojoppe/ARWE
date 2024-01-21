7a3670aeac39b9ec9f9afafc3dc678d0be826006
Joppe Blondel <joppe.blondel@engineero.nl>
2024-01-21 13:10:28 +0100

    Moved to single timer arch

--- cube/Core/Src/tim.c
+++ cube/Core/Src/tim.c
@@ -25,8 +25,6 @@
 /* USER CODE END 0 */
 
 TIM_HandleTypeDef htim6;
-TIM_HandleTypeDef htim14;
-TIM_HandleTypeDef htim16;
 
 /* TIM6 init function */
 void MX_TIM6_Init(void)
@@ -40,9 +38,9 @@ void MX_TIM6_Init(void)
 
   /* USER CODE END TIM6_Init 1 */
   htim6.Instance = TIM6;
-  htim6.Init.Prescaler = 8000;
+  htim6.Init.Prescaler = 80;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 1000;
+  htim6.Init.Period = 100;
   htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
@@ -51,59 +49,6 @@ void MX_TIM6_Init(void)
   /* USER CODE BEGIN TIM6_Init 2 */
 
   /* USER CODE END TIM6_Init 2 */
-
-}
-/* TIM14 init function */
-void MX_TIM14_Init(void)
-{
-
-  /* USER CODE BEGIN TIM14_Init 0 */
-
-  /* USER CODE END TIM14_Init 0 */
-
-  /* USER CODE BEGIN TIM14_Init 1 */
-
-  /* USER CODE END TIM14_Init 1 */
-  htim14.Instance = TIM14;
-  htim14.Init.Prescaler = 8000;
-  htim14.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim14.Init.Period = 200;
-  htim14.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim14.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim14) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM14_Init 2 */
-
-  /* USER CODE END TIM14_Init 2 */
-
-}
-/* TIM16 init function */
-void MX_TIM16_Init(void)
-{
-
-  /* USER CODE BEGIN TIM16_Init 0 */
-
-  /* USER CODE END TIM16_Init 0 */
-
-  /* USER CODE BEGIN TIM16_Init 1 */
-
-  /* USER CODE END TIM16_Init 1 */
-  htim16.Instance = TIM16;
-  htim16.Init.Prescaler = 8000;
-  htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim16.Init.Period = 10000;
-  htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim16.Init.RepetitionCounter = 0;
-  htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim16) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM16_Init 2 */
-
-  /* USER CODE END TIM16_Init 2 */
 
 }
 
@@ -125,36 +70,6 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
 
   /* USER CODE END TIM6_MspInit 1 */
   }
-  else if(tim_baseHandle->Instance==TIM14)
-  {
-  /* USER CODE BEGIN TIM14_MspInit 0 */
-
-  /* USER CODE END TIM14_MspInit 0 */
-    /* TIM14 clock enable */
-    __HAL_RCC_TIM14_CLK_ENABLE();
-
-    /* TIM14 interrupt Init */
-    HAL_NVIC_SetPriority(TIM14_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(TIM14_IRQn);
-  /* USER CODE BEGIN TIM14_MspInit 1 */
-
-  /* USER CODE END TIM14_MspInit 1 */
-  }
-  else if(tim_baseHandle->Instance==TIM16)
-  {
-  /* USER CODE BEGIN TIM16_MspInit 0 */
-
-  /* USER CODE END TIM16_MspInit 0 */
-    /* TIM16 clock enable */
-    __HAL_RCC_TIM16_CLK_ENABLE();
-
-    /* TIM16 interrupt Init */
-    HAL_NVIC_SetPriority(TIM16_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(TIM16_IRQn);
-  /* USER CODE BEGIN TIM16_MspInit 1 */
-
-  /* USER CODE END TIM16_MspInit 1 */
-  }
 }
 
 void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
@@ -173,34 +88,6 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
   /* USER CODE BEGIN TIM6_MspDeInit 1 */
 
   /* USER CODE END TIM6_MspDeInit 1 */
-  }
-  else if(tim_baseHandle->Instance==TIM14)
-  {
-  /* USER CODE BEGIN TIM14_MspDeInit 0 */
-
-  /* USER CODE END TIM14_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM14_CLK_DISABLE();
-
-    /* TIM14 interrupt Deinit */
-    HAL_NVIC_DisableIRQ(TIM14_IRQn);
-  /* USER CODE BEGIN TIM14_MspDeInit 1 */
-
-  /* USER CODE END TIM14_MspDeInit 1 */
-  }
-  else if(tim_baseHandle->Instance==TIM16)
-  {
-  /* USER CODE BEGIN TIM16_MspDeInit 0 */
-
-  /* USER CODE END TIM16_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM16_CLK_DISABLE();
-
-    /* TIM16 interrupt Deinit */
-    HAL_NVIC_DisableIRQ(TIM16_IRQn);
-  /* USER CODE BEGIN TIM16_MspDeInit 1 */
-
-  /* USER CODE END TIM16_MspDeInit 1 */
   }
 }
 
--- src/main.c
+++ src/main.c
@@ -1,106 +1,209 @@
 #include "main.h"
+#include "SEGGER_RTT.h"
 #include "gpio.h"
 #include "tim.h"
-
-#include "SEGGER_RTT.h"
-
 #include <stdbool.h>
 
+#define DEBOUNCE_TIME 100
+#define COOLDOWN_TIME 1000
+#define ON_TIME 1000
+#define HALF_OFF_TIME 5000
+#define QUARTER_OFF_TIME 10000
+
+// ----------------------------------------------------------------------------
+
+enum {
+  SUBTIMER_DEBOUNCE_SWIN = 0,
+  SUBTIMER_DEBOUNCE_SENSE,
+  SUBTIMER_COOLDOWN,
+  SUBTIMER_ONOFF,
+  _SUBTIMER_NUMBER,
+};
+
+enum {
+  STATE_IDLE = 0,
+  STATE_CONSTANT,
+  STATE_HALF,
+  STATE_QUARTER,
+  _STATE_NUMBER,
+};
+
+// ----------------------------------------------------------------------------
+
 extern void SystemClock_Config();
+void SWIN_Changed();
+void SWIN_Debounced();
+void SENSE_Changed();
+void SENSE_Debounced();
+void Cooldown();
+void OnOff();
+
+// ----------------------------------------------------------------------------
+
 extern TIM_HandleTypeDef htim6;
-extern TIM_HandleTypeDef htim14;
-extern TIM_HandleTypeDef htim16;
+static unsigned long long timerValue;
+static unsigned long long timerExpiredValues[_SUBTIMER_NUMBER];
+static void (*timerFunctions[_SUBTIMER_NUMBER])();
+static bool timerUsed[_SUBTIMER_NUMBER];
+static unsigned int state;
+static bool outputState;
 
-typedef enum {
-  SWING_MODE_IDLE,
-  SWING_MODE_FULL,
-  SWING_MODE_HALF_0,
-  SWING_MODE_HALF_1,
-  _SWING_MODE_MAX,
-} swing_mode_t;
-swing_mode_t swing_mode;
-
-typedef enum {
-  HALF_SWINNG_STATE_ON,
-  HALF_SWINNG_STATE_OFF,
-} half_swing_state_t;
-half_swing_state_t half_swing_state;
+// ----------------------------------------------------------------------------
 
 int main() {
   HAL_Init();
   SystemClock_Config();
   MX_GPIO_Init();
   MX_TIM6_Init();
-  MX_TIM14_Init();
-  MX_TIM16_Init();
 
   SEGGER_RTT_Init();
-  SEGGER_RTT_WriteString(0, "Starting ARWE...\r\n");
+  SEGGER_RTT_printf(0, "Starting ARWE...\r\n");
 
-  swing_mode = SWING_MODE_IDLE;
+  state = STATE_IDLE;
+  outputState = false;
 
+  // Reset subtimers
+  timerValue = 0;
+  for (int i = 0; i < _SUBTIMER_NUMBER; i++) {
+    timerExpiredValues[i] = 0;
+    timerFunctions[i] = NULL;
+    timerUsed[i] = false;
+  }
+  // Install subtimer handlers
+  timerFunctions[SUBTIMER_DEBOUNCE_SWIN] = SWIN_Debounced;
+  timerFunctions[SUBTIMER_DEBOUNCE_SENSE] = SENSE_Debounced;
+  timerFunctions[SUBTIMER_COOLDOWN] = Cooldown;
+  timerFunctions[SUBTIMER_ONOFF] = OnOff;
+  // Start millisecond timer
+  HAL_TIM_Base_Start_IT(&htim6);
   for (;;) {
   }
 }
 
-void SWIN_Interrupt() {
-  // Start debouncing
-  HAL_TIM_Base_Start_IT(&htim14);
+// ----------------------------------------------------------------------------
+// EVENTS
+// ----------------------------------------------------------------------------
+
+void SWIN_Debounced() {
+  timerUsed[SUBTIMER_DEBOUNCE_SWIN] = false;
+  // Get current pin state
+  GPIO_PinState state = HAL_GPIO_ReadPin(SWIN_GPIO_Port, SWIN_Pin);
+  if (state) {
+    // Rising edge SWIN
+    // Make sure the cooldown period is stopped
+    timerUsed[SUBTIMER_COOLDOWN] = false;
+    // Increase state by 1 if not already at max
+    if (state < _STATE_NUMBER - 1) {
+      state++;
+    }
+    // Turn on output
+    outputState = true;
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 1);
+    HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, 1);
+    // Determine how to proceed
+    switch (state) {
+    case STATE_CONSTANT:
+      // Make sure the onoff timer is disabled
+      timerUsed[SUBTIMER_ONOFF] = false;
+      break;
+    case STATE_HALF:
+    case STATE_QUARTER:
+      // Start on timer
+      timerExpiredValues[SUBTIMER_ONOFF] = timerValue + ON_TIME;
+      timerUsed[SUBTIMER_ONOFF] = true;
+      break;
+    default:
+      break;
+    }
+  } else {
+    // Falling edge SWIN
+    // Start cooldown period
+    timerExpiredValues[SUBTIMER_COOLDOWN] = timerValue + COOLDOWN_TIME;
+    timerUsed[SUBTIMER_COOLDOWN] = true;
+  }
+}
+
+void SENSE_Debounced() {
+  timerUsed[SUBTIMER_DEBOUNCE_SENSE] = false;
+  // Get current pin state
+  GPIO_PinState state = HAL_GPIO_ReadPin(SENSE_GPIO_Port, SENSE_Pin);
+  if (state) {
+    // Rising edge SENSE
+  } else {
+    // Falling edge SENSE
+  }
+}
+
+void Cooldown() {
+  timerUsed[SUBTIMER_COOLDOWN] = false;
+  // Cooldown period expired: reset state back to idle
+  state = STATE_IDLE;
+  timerUsed[SUBTIMER_ONOFF] = false;
+  outputState = false;
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 0);
+  HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, 0);
+}
+
+void OnOff() {
+  timerUsed[SUBTIMER_ONOFF] = false;
+  if (outputState) {
+    // Turn the output off
+    outputState = false;
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 0);
+    HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, 0);
+    // Set off timer
+    switch (state) {
+    case STATE_HALF:
+      timerExpiredValues[SUBTIMER_ONOFF] = timerValue + HALF_OFF_TIME;
+      break;
+    case STATE_QUARTER:
+      timerExpiredValues[SUBTIMER_ONOFF] = timerValue + QUARTER_OFF_TIME;
+      break;
+    default:
+      break;
+    }
+    timerUsed[SUBTIMER_ONOFF] = true;
+  } else {
+    // Turn the output on
+    outputState = true;
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 1);
+    HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, 1);
+    // Set on timer
+    timerExpiredValues[SUBTIMER_ONOFF] = timerValue + ON_TIME;
+    timerUsed[SUBTIMER_ONOFF] = true;
+  }
+}
+
+// ----------------------------------------------------------------------------
+// HARDWARE INTERRUPTS
+// ----------------------------------------------------------------------------
+
+void SWIN_Changed() {
+  // Start debouncing if not already busy
+  if (!timerUsed[SUBTIMER_DEBOUNCE_SWIN]) {
+    timerExpiredValues[SUBTIMER_DEBOUNCE_SWIN] = timerValue + DEBOUNCE_TIME;
+    timerUsed[SUBTIMER_DEBOUNCE_SWIN] = true;
+  }
+}
+
+void SENSE_Changed() {
+  // Start debouncing if not already busy
+  if (!timerUsed[SUBTIMER_DEBOUNCE_SENSE]) {
+    timerExpiredValues[SUBTIMER_DEBOUNCE_SENSE] = timerValue + DEBOUNCE_TIME;
+    timerUsed[SUBTIMER_DEBOUNCE_SENSE] = true;
+  }
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
   if (htim->Instance == TIM6) {
-    // MODE SELECT TIMER INTERRUPT -> OFF
-    HAL_TIM_Base_Stop_IT(&htim6);
-    HAL_TIM_Base_Stop_IT(&htim16);
-    swing_mode = SWING_MODE_IDLE;
-    SEGGER_RTT_printf(0, "E tim6: %d\r\n", swing_mode);
-    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 0);
-    HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, 0);
-
-  } else if (htim->Instance == TIM14) {
-    // DEBOUNCE TIMER INTERRUPT
-    HAL_TIM_Base_Stop_IT(&htim14);
-
-    int val = HAL_GPIO_ReadPin(SWIN_GPIO_Port, SWIN_Pin);
-    if (!val) {
-      // Rising SWIN
-      if (swing_mode < _SWING_MODE_MAX - 1) {
-        swing_mode++;
+    // Millisecond timer
+    timerValue++;
+    // Check subtimers and call if expired
+    for (int i = 0; i < _SUBTIMER_NUMBER; i++) {
+      if (timerUsed[i] && timerExpiredValues[i] == timerValue &&
+          timerFunctions[i] != NULL) {
+        timerFunctions[i]();
       }
-      HAL_TIM_Base_Stop_IT(&htim6);
-      SEGGER_RTT_printf(0, "^ swin: %d\r\n", swing_mode);
-      HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 1);
-      HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, 1);
-      if (swing_mode >= SWING_MODE_HALF_0) {
-        // Start pulsed mode
-        htim16.Init.Period = 1000;
-        HAL_TIM_Base_Init(&htim16);
-        HAL_TIM_Base_Start_IT(&htim16);
-        half_swing_state = HALF_SWINNG_STATE_ON;
-      }
-    } else {
-      // Falling SWIN
-      HAL_TIM_Base_Start_IT(&htim6);
-      SEGGER_RTT_printf(0, "_ swin: %d\r\n", swing_mode);
-    }
-
-  } else if (htim->Instance = TIM16) {
-    // OUTPUT TIMER INTERRUPT
-    if (half_swing_state == HALF_SWINNG_STATE_ON) {
-      HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 0);
-      HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, 0);
-      half_swing_state = HALF_SWINNG_STATE_OFF;
-      if (swing_mode == SWING_MODE_HALF_0) {
-        __HAL_TIM_SET_AUTORELOAD(&htim16, 5000);
-      } else if (swing_mode == SWING_MODE_HALF_1) {
-        __HAL_TIM_SET_AUTORELOAD(&htim16, 10000);
-      }
-    } else {
-      HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 1);
-      HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, 1);
-      half_swing_state = HALF_SWINNG_STATE_ON;
-      __HAL_TIM_SET_AUTORELOAD(&htim16, 1000);
     }
   }
 }
@@ -108,9 +211,14 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
   switch (GPIO_Pin) {
   case SWIN_Pin:
-    SWIN_Interrupt();
+    SWIN_Changed();
+    break;
+  case SENSE_Pin:
+    SENSE_Changed();
     break;
   default:
     break;
   }
 }
+
+// ----------------------------------------------------------------------------
